590b9c52467ca8c511e421e44246992ac8ea0a5f
alexeygold2077 <alexgold2077@yandex.ru>
2024-06-20 15:33:23 +0300

    added discription to ByBit class

--- src/main/java/com/alexeygold2077/api/ByBit.java
+++ src/main/java/com/alexeygold2077/api/ByBit.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 
 class ByBit {
 
+    // Get the spec of symbol information
     public static String getSymbols() throws IOException {
 
         final String BASE_URL = "https://api.bybit.com/spot/v3/public/symbols";
@@ -27,6 +28,7 @@ class ByBit {
         return responseBody;
     }
 
+    // 24hr ticker data
     public static String getTicker(String symbol) throws IOException {
 
         final String BASE_URL = "https://api.bybit.com/spot/v3/public/quote/ticker/24hr";
