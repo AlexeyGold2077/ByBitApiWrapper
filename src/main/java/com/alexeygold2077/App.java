f55146f476ba07b0777c0a748542ae2f42cafec1
alexeygold2077 <alexgold2077@yandex.ru>
2024-06-20 13:01:06 +0300

    edit gitignore, initialized maven

---
+++ src/main/java/com/alexeygold2077/App.java
@@ -0,0 +1,13 @@
+package com.alexeygold2077;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
