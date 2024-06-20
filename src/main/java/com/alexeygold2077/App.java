636a913c9958b2e4b113468abd423552346bfc47
alexeygold2077 <alexgold2077@yandex.ru>
2024-06-20 13:04:40 +0300

    edit pom.xml

--- src/main/java/com/alexeygold2077/App.java
+++ src/main/java/com/alexeygold2077/App.java
@@ -1,13 +1,7 @@
 package com.alexeygold2077;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
