36be49f233f994d5300075917b1c51463f1fd72e
alexeygold2077 <alexgold2077@yandex.ru>
2024-06-21 10:40:18 +0300

    cleaned some code

--- src/main/java/com/alexeygold2077/api/ByBitWrapper.java
+++ src/main/java/com/alexeygold2077/api/ByBitWrapper.java
@@ -13,13 +13,6 @@ public class ByBitWrapper {
     private ByBit byBit;
     private ObjectMapper objectMapper;
 
-    public ByBitWrapper() {}
-
-    public ByBitWrapper(ByBit byBit, ObjectMapper objectMapper) {
-        this.byBit = byBit;
-        this.objectMapper = objectMapper;
-    }
-
     // Get the price of a single symbol
     public float getTickerPrice(String symbol) throws IOException, ByBitException {
 
@@ -52,25 +45,15 @@ public class ByBitWrapper {
         return availableTickers;
     }
 
-    public float get_BTCUSDT_Price() throws IOException, ByBitException {
-        return getTickerPrice("BTCUSDT");
-    }
+    public float get_BTCUSDT_Price() throws IOException, ByBitException { return getTickerPrice("BTCUSDT"); }
 
-    public float get_ETHUSDT_Price() throws IOException, ByBitException {
-        return getTickerPrice("ETHUSDT");
-    }
+    public float get_ETHUSDT_Price() throws IOException, ByBitException { return getTickerPrice("ETHUSDT"); }
 
-    public float get_TONUSDT_Price() throws IOException, ByBitException {
-        return getTickerPrice("TONUSDT");
-    }
+    public float get_TONUSDT_Price() throws IOException, ByBitException { return getTickerPrice("TONUSDT"); }
 
-    public float get_NOTUSDT_Price() throws IOException, ByBitException {
-        return getTickerPrice("NOTUSDT");
-    }
+    public float get_NOTUSDT_Price() throws IOException, ByBitException { return getTickerPrice("NOTUSDT"); }
 
-    public float get_DOGEUSDT_Price() throws IOException, ByBitException {
-        return getTickerPrice("DOGEUSDT");
-    }
+    public float get_DOGEUSDT_Price() throws IOException, ByBitException { return getTickerPrice("DOGEUSDT"); }
 
     private void checkException(int retCode) throws ByBitException {
         if (retCode == 12810)
